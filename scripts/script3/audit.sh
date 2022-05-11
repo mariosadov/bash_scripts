522f708ad73c83219811dbe9541380d82682d17f
mariosadov <93256729+mariosadov@users.noreply.github.com>
2022-05-11 23:39:38 +0300

    Add files via upload

--- scripts/script3/audit.sh
+++ scripts/script3/audit.sh
@@ -1,33 +1,33 @@
 #! /bin/bash
 
 # write uptime into file
-printf "uptime:\n" > audit_result.txt
-uptime >> audit_result.txt
+printf "uptime:\n" > audit_result.log
+uptime >> audit_result.log
 
 # write current users into file
-printf "\ncurrent users:\n" >> audit_result.txt
-users >> audit_result.txt
+printf "\ncurrent users:\n" >> audit_result.log
+users >> audit_result.log
 
 # write 10 recent logins into file
-printf "\nmost recent logins:\n" >> audit_result.txt
-last | head -n 10 >> audit_result.txt
+printf "\nmost recent logins:\n" >> audit_result.log
+last | head -n 10 >> audit_result.log
 
 # write top 10 proceses by ram  into file
-printf "\ntop 10 processes by ram:\n" >> audit_result.txt
-ps -aux --sort=-%mem | head -n 11 >> audit_result.txt
+printf "\ntop 10 processes by ram:\n" >> audit_result.log
+ps -aux --sort=-%mem | head -n 11 >> audit_result.log
 
 # write top 10 proceses by cpu  into file
-printf "\ntop 10 processes by cpu:\n" >> audit_result.txt
-ps -aux --sort=-%cpu | head -n 11 >> audit_result.txt
+printf "\ntop 10 processes by cpu:\n" >> audit_result.log
+ps -aux --sort=-%cpu | head -n 11 >> audit_result.log
 
 # write open ports into file
-printf "\nopen ports:\n" >> audit_result.txt
-netstat -tulpn | grep LISTEN >> audit_result.txt
+printf "\nopen ports:\n" >> audit_result.log
+netstat -tulpn | grep LISTEN >> audit_result.log
 
 # write virtual memory usage into file
-printf "\nvirtual memory info:\n" >> audit_result.txt
-free -h >> audit_result.txt
+printf "\nvirtual memory info:\n" >> audit_result.log
+free -h >> audit_result.log
 
 # write disk memory usage into file
-printf "\ndisk memory usage info:\n" >> audit_result.txt
-du -h -s /* >> audit_result.txt 2>audit_err.txt
+printf "\ndisk memory usage info:\n" >> audit_result.log
+du -h -s /* >> audit_result.log 2>audit_err.log
